8e739445f381ad5c6cc9a89d6689ce279139570f
annmunyare <ann.munyare@nanyukiappfactory.onmicrosoft.com>
2018-11-08 11:38:56 +0300

    m

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,7 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::group(['prefix' => 'api/v1'], function() {
+Route::group(['prefix' => 'v1'], function() {
 
     Route::resource('flows', 'FlowController');
    });
