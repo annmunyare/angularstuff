0f92a475a8e689d2dcd14dfbba01f8a731554688
annmunyare <ann.munyare@nanyukiappfactory.onmicrosoft.com>
2018-11-15 00:08:17 +0300

    messagecontroller adde

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -18,6 +18,11 @@ class Cors
         return $next($request)
         ->header('Access-Control-Allow-Origin', '*')
         ->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, PATCH, DELETE, OPTIONS')
-        ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Auth-Token, Origin');
+
+   
     }
+
+
+
 }
