c7b2073b97172a9badaba0da901e2f0d8408d870
annmunyare <ann.munyare@nanyukiappfactory.onmicrosoft.com>
2018-11-22 10:53:57 +0300

    n

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -44,6 +44,7 @@ class Kernel extends HttpKernel
             'throttle:60,1',
             'bindings',
             '\Barryvdh\Cors\HandleCors::class',
+           
         ],
     ];
 
@@ -64,7 +65,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'cors' => \Barryvdh\Cors\HandleCors::class,
+    
     ];
 
     /**
